85f0ce3ca589a367ce2a4d62628459a1754263e8
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-06-12 18:59:38 +0200

    cors

--- server.js
+++ server.js
@@ -15,20 +15,20 @@ mongoose.Promise = global.Promise;
 app.use(morgan('common'));
 
 // CORS
-app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
-);
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-//   if (req.method === 'OPTIONS') {
-//     return res.send(204);
-//   }
-//   next();
-// });
+// app.use(
+//   cors({
+//     origin: CLIENT_ORIGIN
+//   })
+// );
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
