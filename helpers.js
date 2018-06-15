408779f06a237e38f4c6c437404985051b138403
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-06-15 11:56:19 +0200

    narrowing priceData results

--- helpers.js
+++ helpers.js
@@ -1,4 +1,21 @@
 const moment = require('moment');
+const coinWatchList = [
+  'BTC',
+  'ETH',
+  'XRP',
+  'BCH',
+  'EOS',
+  'LTC',
+  'ADA',
+  'XLM',
+  'DASH',
+  'DOGE',
+  'XEM',
+  'XMR'
+];
+// BCH first date: 2017-07-27
+// EOS first date: 2017-06-28
+// ADA first date: 2017-10-01
 
 function getStartDate(date) {
   return moment(date)
@@ -7,22 +24,23 @@ function getStartDate(date) {
 }
 
 function parsePriceData(data) {
-  //   const priceData = [];
   const priceData = data.map(coin => {
-    const coinDatum = {
-      currency: coin.currency,
-      current: coin.close
-    };
-    const momentA = moment(coin.close_timestamp);
-    const momentB = moment(coin.open_timestamp);
-    // Check if coin price seven days earlier exists
-    if (momentA.diff(momentB, 'days') === 7) {
-      coinDatum.sevenDaysAgo = coin.open;
-    } else {
-      coinDatum.sevenDaysAgo = 'N/A';
-    }
+    if (coinWatchList.includes(coin.currency)) {
+      const coinDatum = {
+        currency: coin.currency,
+        current: coin.close
+      };
+      const momentA = moment(coin.close_timestamp);
+      const momentB = moment(coin.open_timestamp);
+      // Check if coin price seven days earlier exists
+      if (momentA.diff(momentB, 'days') === 7) {
+        coinDatum.sevenDaysAgo = coin.open;
+      } else {
+        coinDatum.sevenDaysAgo = 'N/A';
+      }
 
-    return coinDatum;
+      return coinDatum;
+    }
   });
 
   return priceData;
