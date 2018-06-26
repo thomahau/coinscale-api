39e8168f68ba72e2c78e2f118c90d946631de473
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-06-26 07:15:47 +0200

    client origin

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
-// const cors = require('cors');
 
-const { PORT, DATABASE_URL } = require('./config');
+const { PORT, DATABASE_URL, CLIENT_ORIGIN } = require('./config');
 const { localStrategy, jwtStrategy } = require('./auth');
 
 const app = express();
@@ -16,7 +15,7 @@ app.use(morgan('common'));
 
 // CORS
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', CLIENT_ORIGIN);
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
@@ -24,11 +23,6 @@ app.use(function(req, res, next) {
   }
   next();
 });
-// app.use(
-//   cors({
-//     origin: CLIENT_ORIGIN
-//   })
-// );
 
 passport.use(localStrategy);
 passport.use(jwtStrategy);
